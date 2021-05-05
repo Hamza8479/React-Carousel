dd675e6433747692ee6f225f26638cefd107b1c8
Hamza <hamzazaman500@gmail.com>
2021-05-05 23:39:46 +0500

    slider

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import './App.css';
 import ImageSlider from './components/ImageSlider';
-import {
-  Data
-} from './components/SliderData'
+import {Data} from './components/SliderData'
 
 function App() {
   return (
-    h
-    ImageSlider slides = {
-      Data
-    }
-    />
+      <div className='container'>
+      <div className='header'> 
+      
+      <div>
+        <ul>
+          <li>asd</li>
+          <li>as</li>
+          <li>asda</li>
+        </ul>
+      </div>
 
+      </div>
+      
+      <ImageSlider slides={Data}/>
+
+      <h1>Hamza </h1>
+      </div>
+  
   );
 }
 
--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -1,6 +1,6 @@
 export const Data = [
     {
-        image:'https://lh3.googleusercontent.com/proxy/vSP2Rdt4Em_ACA8H9BUM1Jq9fIxpU2rWbUHrfXfDQN7zZVdFM6NX445jgcSJe2ct2RW96kbtIZPSyM6o0XX8xCspHwFPBHmrvNTdkGAj5GrkmaBwjEec82fJWg'
+        image:'http://cdn.mos.cms.futurecdn.net/6t8Zh249QiFmVnkQdCCtHK.jpg'
     },
     {
         image:'https://i.pinimg.com/originals/69/e6/e5/69e6e51f073c3f3a6b0b160e828ba9fe.jpg'
